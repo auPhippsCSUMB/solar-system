8b26e8c4770e7c4b508bd5184d3ec2a4deeffc1c
auPhippsCSUMB <auphipps@csumb.edu>
2026-03-22 19:51:44 -0700

    added comets, asteroids, fixed NASA POD

--- index.mjs
+++ index.mjs
@@ -34,13 +34,30 @@ app.get('/planetInfo', (req, res) => {
 });
 
 app.get('/planets', async(req, res) => {
-    let url = "https://api.nasa.gov/planetary/apod?api_key=9mUzIkhlZCZaOoMfspg7jMmwZCZ4LiRHtkgkambD&date=2026-03-11";
+    let date = new Date();
+    let today = date.getDate();
+    let url = "https://api.nasa.gov/planetary/apod?api_key=9mUzIkhlZCZaOoMfspg7jMmwZCZ4LiRHtkgkambD&date=" + date.toISOString().substring(0, 8) + today;
     let response = await fetch(url);
     let data = await response.json();
     let img = data.url;
-    res.render('planets.ejs', {img});
-})
+    let imgText;
+    if (img.substring(img.length-3, img.length) == "mp4") {
+        console.log("ITS MP4");
+        imgText = img;
+    }
+    console.log(img);
+    res.render('planets.ejs', {img, imgText});
+});
 
+app.get('/asteroids', async(req, res) => {
+    let asteroids = planets.getAsteroids();
+    res.render('asteroids.ejs', {asteroids});
+});
+
+app.get('/comets', async(req, res) => {
+    let comets = planets.getComets();
+    res.render('comets.ejs', {comets});
+});
 
 app.listen(3000, () => {
     console.log('server started');
