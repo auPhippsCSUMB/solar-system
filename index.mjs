755f635305a1771bbb7bfd5c56eb4d7526415cd7
auPhippsCSUMB <auphipps@csumb.edu>
2026-03-22 20:33:09 -0700

    ignoring all mp4 scenarios

--- index.mjs
+++ index.mjs
@@ -42,14 +42,14 @@ app.get('/planets', async(req, res) => {
     let img = await data.url;
     let imgText;
 
-    if (!img) {
-        return res.render('planets.ejs', { img: null, imgText: null });
-    }
+    // if (!img) {
+    //     return res.render('planets.ejs', { img: null, imgText: null });
+    // }
 
-    if (img.substring(img.length-3, img.length) == "mp4") {
-        console.log("ITS MP4");
-        imgText = img;
-    }
+    // if (img.substring(img.length-3, img.length) == "mp4") {
+    //     console.log("ITS MP4");
+    //     imgText = img;
+    // }
     console.log(img);
     res.render('planets.ejs', {img, imgText});
 });
